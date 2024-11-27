61943847e3f77d59d46542bd67cd89cb46c17dc7
sina hakimzadeh <sina.hakimzadeh.1401@gmail.com>
2024-11-27 16:51:17 +0330

    parallelize each image process

--- main.c
+++ main.c
@@ -1,60 +1,95 @@
 #include "utilities.h"
+#include "pthread.h"
 
 #define INPUT_FILE_NAME "input/in%d.bmp"
 #define OUTPUT_FILE_NAME "output/out%d.bmp"
 
-int main(int argc, char *argv[])
-{
-	int bodySize;
+void *processImage(void *imageNum){
+    int bodySize;
 	int width, height;
 
-	double time_spent;
-    double cummulativetime = 0;
+    int index = (int) imageNum;
+    char inputFile[15];
+    char outputFilename[20];
+
+    clock_t start,end;
+    int time_spent;
+    ImageData *imageData = (ImageData *) malloc(sizeof(ImageData));
+
 
     unsigned char *header = NULL;
 	unsigned char *body = NULL;
 	unsigned char *results = NULL;
 
-	clock_t start, end;
-    ImageData *imageData = (ImageData *) malloc(NUMBER_OF_INPUTS * sizeof(ImageData));
-    
-    char inputFile[15];
-    char outputFilename[20];
+    sprintf(inputFile, INPUT_FILE_NAME, index);
 
-
-    for (int index = 1; index <= NUMBER_OF_INPUTS; index++) {
-        sprintf(inputFile, INPUT_FILE_NAME, index);
-
-        int statusOpeningImage = OpenImage(inputFile, &header, &body, &bodySize, &width, &height);
-	    if (statusOpeningImage != OPENING_IMAGE_SUCCESS) {
-		    printf("Error: An earlier error occured with the given description.\n");
-		    return statusOpeningImage;
-	    }
-        
-        imageData[index - 1].grayValues = body;
-        imageData[index - 1].size = bodySize;
-
-        
-        start = clock();
-        ConvertRGBtoGrayscale((void *) &imageData[index - 1]);
-        end = clock();
-
-
-        time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-        printf("Time spent on input %d: %f microseconds \n", index, time_spent * 1000);
-
-        cummulativetime += time_spent;
-        
-        sprintf(outputFilename, OUTPUT_FILE_NAME, index);
-        WriteImage(outputFilename, header, body, bodySize);
+    int statusOpeningImage = OpenImage(inputFile, &header, &body, &bodySize, &width, &height);
+    if (statusOpeningImage != OPENING_IMAGE_SUCCESS) {
+        printf("Error: An earlier error occured with the given description.\n");
     }
+    
+    (*imageData).grayValues = body;
+    (*imageData).size = bodySize;
+
+    
+    start = clock();
+    ConvertRGBtoGrayscale((void *) imageData);
+    end = clock();
+
+
+    time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time spent on input %d: %f microseconds \n", index, time_spent * 1000);
+
+    // cummulativetime += time_spent;
+    
+    sprintf(outputFilename, OUTPUT_FILE_NAME, index);
+    WriteImage(outputFilename, header, body, bodySize);
 
     free(results);
 	free(header);
 	free(body);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    int rc;
+    void *status;
+
+	double time_spent;
+    double cummulativetime = 0;
+
+    // unsigned char *header = NULL;
+	// unsigned char *body = NULL;
+	// unsigned char *results = NULL;
+
+	clock_t start, end;
+    
+    pthread_t threads[NUMBER_OF_INPUTS];
 
 
-    printf("Total Time Spent: %f microseconds \n", cummulativetime * 1000);
+    for (int index = 1; index <= NUMBER_OF_INPUTS; index++) {
+        rc = pthread_create(&threads[index], NULL, processImage, (void *)index);
+        if (rc){
+            printf("Error; return code from pthread_creat() is %d \n",rc);
+            exit(-1);
+        }
+    }
+
+    for (int index = 1; index <= NUMBER_OF_INPUTS; index++){
+        rc = pthread_join(threads[index], &status);
+        if (rc){
+            printf("Error; return code from pthread_join() is %d \n",rc);
+            exit(-1);
+        }
+        printf("Complete join with thread %d \n", (long)status);
+        }
+    pthread_exit(NULL);
+
+   
+
+
+    // printf("Total Time Spent: %f microseconds \n", cummulativetime * 1000);
 
     return 0;
 }
